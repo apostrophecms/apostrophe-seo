28a6ba9d58ccfc8ab120734d60fd17c8433538fd
Brad Wood <woodbr@earlham.edu>
2019-10-31 13:52:40 -0400

    improve apostrophe-custom-pages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   moogBundle: {
     modules: [
-      'apostrophe-seo-doc-type-manager',
+      'apostrophe-seo-custom-pages',
       'apostrophe-seo-files',
       'apostrophe-seo-images',
       'apostrophe-seo-global',
