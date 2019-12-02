44adadfbf5c307ac619e7c2d6d92160819600b8e
Alex Bea <alex.bea@gmail.com>
2019-12-02 10:20:06 -0600

    Moves canonical field to custom pages improver

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   moogBundle: {
     modules: [
       'apostrophe-seo-doc-type-manager',
+      'apostrophe-seo-custom-pages',
       'apostrophe-seo-files',
       'apostrophe-seo-images',
       'apostrophe-seo-global',
