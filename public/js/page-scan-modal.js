9da43c7ffdc261d1d6539f97ed288532bdfd3228
Val <valentin.jeudy@gmail.com>
2021-01-28 11:19:20 +0100

    add client files for page scanner modal

---
+++ public/js/page-scan-modal.js
@@ -0,0 +1,8 @@
+apos.define('page-scan-modal', {
+  extend: 'apostrophe-modal',
+  source: 'page-scan-modal',
+
+  construct (self, options) {
+    console.log('=============> MODAL CONSTRUCT <================');
+  }
+});
---
+++ public/js/user.js
@@ -0,0 +1,14 @@
+apos.define('apostrophe-seo', {
+  extend: 'apostrophe-context',
+
+  construct: function(self, options) {
+    self.scan = function(options) {
+      return apos.create('page-scan-modal', {
+        action: self.action,
+        body: {
+          href: window.location.href
+        }
+      });
+    };
+  }
+});
